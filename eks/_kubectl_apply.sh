3381f76d9a8dea6be1f4100a7ff2d9982d8d2924
lvdiev <48303941+lvdiev@users.noreply.github.com>
2023-03-01 16:20:44 +0000

    add deployments

---
+++ eks/_kubectl_apply.sh
@@ -0,0 +1,20 @@
+# update config
+aws eks --region us-east-1 update-kubeconfig --name udagram-eks-cluster
+
+# env variables and secrets
+kubectl apply -f env/aws-secret.yaml
+kubectl apply -f env/env-secret.yaml
+kubectl apply -f env/env-configmap.yaml
+
+# deployments and services
+kubectl apply -f feed-deployment.yaml
+kubectl apply -f feed-service.yaml
+
+kubectl apply -f user-deployment.yaml
+kubectl apply -f user-service.yaml
+
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
+
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
