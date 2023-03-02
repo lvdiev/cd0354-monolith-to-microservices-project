2607d51831c10eb15370b7ac9dc27ee4e49a4f50
lvdiev <48303941+lvdiev@users.noreply.github.com>
2023-03-02 09:19:55 +0000

    update kube delete scripts

--- eks/_kubectl_delete.sh
+++ eks/_kubectl_delete.sh
@@ -1,3 +1,9 @@
 # un-deployments
+kubectl delete deployment udagram-api-feed
+kubectl delete service udagram-api-feed
+
+kubectl delete deployment udagram-api-user
+kubectl delete service udagram-api-user
+
 kubectl delete --all service
 kubectl delete --all deployment
