6293d921a4fa8624b09522035889438acfe5d240
lvdiev <48303941+lvdiev@users.noreply.github.com>
2023-03-02 05:55:19 +0000

    fix deployment files

--- eks/_kubectl_apply.sh
+++ eks/_kubectl_apply.sh
@@ -13,8 +13,15 @@ kubectl apply -f feed-service.yaml
 kubectl apply -f user-deployment.yaml
 kubectl apply -f user-service.yaml
 
-kubectl apply -f frontend-deployment.yaml
-kubectl apply -f frontend-service.yaml
+# kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f frontend-service.yaml
 
 kubectl apply -f reverseproxy-deployment.yaml
 kubectl apply -f reverseproxy-service.yaml
+
+# expose services
+kubectl expose service udagram-reverseproxy --type=LoadBalancer  --name=public-reserveproxy --port=80 --target-port=8080
+
+# un-deployments
+# kubectl delete --all service
+# kubectl delete --all deployment
