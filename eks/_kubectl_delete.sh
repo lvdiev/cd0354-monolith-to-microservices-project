14d0f4428a4a900c9795b5f338908a4e43523ad0
lvdiev <48303941+lvdiev@users.noreply.github.com>
2023-03-02 09:42:46 +0000

    more delete scripts

--- eks/_kubectl_delete.sh
+++ eks/_kubectl_delete.sh
@@ -5,5 +5,12 @@ kubectl delete service udagram-api-feed
 kubectl delete deployment udagram-api-user
 kubectl delete service udagram-api-user
 
+kubectl delete service udagram-frontend
+kubectl delete service udagram-frontend
+
+kubectl delete deployment udagram-reverseproxy
+kubectl delete service udagram-reverseproxy
+kubectl delete service public-reserveproxy
+
 kubectl delete --all service
 kubectl delete --all deployment
